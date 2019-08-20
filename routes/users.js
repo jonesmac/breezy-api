b1f93df0a0675d000dfd25dff5b03a559d990d75
Matt <jones@railroad19.com>
2019-08-20 09:26:05 -0400

    add nodemon, stub user routes, cors

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var dummyData = require('../public/fake-users.json');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(dummyData);
 });
+
+router.post('/', function (req, res, next) {
+  res.status(201).send({ success: 'User Successfully Created'});
+})
 
 module.exports = router;
