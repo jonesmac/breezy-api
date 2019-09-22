00cc7281b1015e53820bed4025a549d2bbfb6621
Matt <jones@railroad19.com>
2019-09-22 16:45:10 -0400

    move location query to scope

--- components/users/index.js
+++ components/users/index.js
@@ -8,20 +8,9 @@ module.exports = {
     return User.findOne({ where: { email } });
   },
   locations: (user) => {
-    /**
-     * Here we are finding all Locations and including the
-     * relationship to Users as 'users' since the model defines
-     * the relationship that way and only including results where 
-     * the user's id matched what we passed in and finally not 
-     * including any of the user's attributes for clean output.
-     */
-    return Location.findAll({
-      include: [{
-        model: User,
-        as: 'users',
-        where: { id: user.id },
-        attributes: []
-       }]
-    });
+    return Location.scope({ method: ['forUser', user]}).findAll();
+  },
+  location: (user, id) => {
+    return Location.scope({ method: ['forUser', user, id]}).findOne();
   }
 }
